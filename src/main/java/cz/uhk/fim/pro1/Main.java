eda012191ffbcee13019b323eaf5d5c6d5cac5bf
labas02 <zdenda.cejka112@seznam.cz>
2026-05-06 14:06:21 +0200

    added input for options tui only

--- src/main/java/cz/uhk/fim/pro1/Main.java
+++ src/main/java/cz/uhk/fim/pro1/Main.java
@@ -2,10 +2,13 @@ package cz.uhk.fim.pro1;
 
 import cz.uhk.fim.pro1.model.Fakulta;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+    void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         IO.println(String.format("Vitejte na FIM!"));
@@ -14,7 +17,29 @@ public class Main {
 //        Arrays.asList(Semestr.values()).forEach((s) -> { list.add(s.toString()); });
 
         Fakulta fakulta = new Fakulta("Fakulta informatiky a managementu", "FIM");
-        System.out.println(fakulta);
+        print_help();
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            switch (input){
+                case "1":
+                    System.out.println(fakulta);
+                    break;
+                case "2":
+                    System.out.println(fakulta);
+                    break;
+                case "3":
+                    print_help();
+                    break;
+                default:
+            }
+        }
 
+    }
+
+    void print_help(){
+        System.out.println("statistika ucitele: 1\n" +
+                "print fakulta: 2\n" +
+                "help: 3");
     }
 }
